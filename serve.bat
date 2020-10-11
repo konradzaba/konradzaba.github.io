0413426b32e6f3cc0117b0e2efe25f5e22b5a5e0
Konrad Żaba <elzabbul@gmail.com>
2020-10-11 23:48:19 +0200

    sitemap fix

--- serve.bat
+++ serve.bat
@@ -1,2 +1,3 @@
 copy _site\sitemap.xml sitemap.xml
+powershell.exe "((Get-Content -path sitemap.xml -Raw) -replace 'http://localhost:4000/','https://konradzaba.github.io/') | Set-Content -Path sitemap.xml"
 bundle exec jekyll serve
